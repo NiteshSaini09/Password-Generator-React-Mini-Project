3bd1766d8f700599f01984a21e26d20316299f76
Nitesh Saini <nitesh.tech.dev@gmail.com>
2026-09-22 18:36:06 +0530

    password generation functionality and UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,97 @@
-import { useState,useCallback } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import { useState, useCallback, useEffect, useRef } from "react";
+import heroImg from "./assets/hero.png";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import "./App.css";
 
 function App() {
-  const [length, setLength] = useState(6)
-  const [numberAllowed, setNumberAllowed]=useState(false)
-  const [CharAllowed, setCharAllowed]=useState(false)
-  const [password,setPassword]=useState("")
-  const generatePassword=useCallback(()=>{
-    let pass=""
-    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    if(numberAllowed)str+="1234567890"
-    if(CharAllowed)str+="!@#$%^&*-_,./"
-    for(let i=1;i<=str.length;i++){
-      let charIndex=Math.floor(Math.random()*str.length +1)
-      pass+=str.charAt(charIndex)
+  const [length, setLength] = useState(6);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [CharAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+  const generatePassword = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "1234567890";
+    if (CharAllowed) str += "!@#$%^&*-_,./";
+    for (let i = 1; i <= length; i++) {
+      let charIndex = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(charIndex);
     }
-    setPassword(pass)
+    setPassword(pass);
+  }, [length, numberAllowed, CharAllowed]);
 
-  },[length,numberAllowed,CharAllowed])
+  useEffect(() => {
+    generatePassword();
+  }, [length, numberAllowed, CharAllowed, generatePassword]);
+  //useRef Hook for
+  // Copy Password
+  const passRef = useRef(null);
+  const copyPass = useCallback(() => {
+    passRef.current?.select()
+    window.navigator.clipboard.writeText(password);
+  },[password]);
 
-
+  // console.log(passRef.current.value);
   return (
-   <>
-    <h1 className='text-3xl mt-20 text-center'>Password Generator</h1>
-   </>
-  )
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-600">
+      <h1 className="text-3xl text-center">Password Generator</h1>
+      <div className="flex shadow rounded-lg overflow-hidden p-5">
+        <input
+          type="text"
+          value={password}
+          className="outline-none w-full px-3 bg-white text-black"
+          placeholder="password"
+          readOnly
+          ref={passRef}
+        />
+        <button
+          className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+          onClick={copyPass}
+        >
+          copy
+        </button>
+      </div>
+      <div className="flex text-sm gap-x-2">
+        <div className="flex items-center gap-x-1">
+          <input
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => {
+              setLength(e.target.value);
+            }}
+          />
+          <label>Length: {length}</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={() => {
+              setNumberAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={CharAllowed}
+            id="characterInput"
+            onChange={() => {
+              setCharAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="characterInput">Characters</label>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
